2eb63b7a04fd467d8ce9e7fe3d411caefbca0b8a
rahim <rahim.aghareb@gmail.com>
2022-08-07 09:09:13 +0430

    end of lesson 6

--- course/views.py
+++ course/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .models import Course
-from .serializers import CourseListSerializer
+from .models import Course, Lesson
+from .serializers import CourseListSerializer, CourseDetailSerializer, LessonListSerializer
 
 @api_view(['GET'])
 def get_courses(request):
@@ -11,3 +11,18 @@ def get_courses(request):
     #  because it is multiple objects we pass many = True
     serializer = CourseListSerializer(courses, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_course(request, slug):
+    course = Course.objects.get(slug=slug)
+    course_serializer = CourseDetailSerializer(course)
+    lesson_serializer = LessonListSerializer(course.lessons.all(), many=True) #many = true because it has more object than 1
+
+    data = {
+        'course': course_serializer.data,
+        'lessons': lesson_serializer.data
+    }
+    return Response(data)
+
+
