7c0eeca33e9b065c5844fee31b6bf7568126e354
rahim <rahim.aghareb@gmail.com>
2023-05-31 15:32:17 +0330

    update api site

--- studynet_django/settingsprod.py
+++ studynet_django/settingsprod.py
@@ -2,6 +2,14 @@ from .settings import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['aghareb.ir']
+ALLOWED_HOSTS = ['api.aghareb.ir']
 # settings to show images of course thumbnail
-WEBSITE_URL = 'http://aghareb.ir'
+WEBSITE_URL = 'http://api.aghareb.ir'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://aghareb.ir",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://aghareb.ir",
+]
